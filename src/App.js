a3ee8b9482c0b61d96be665b54268b5b332e1768
zharkyn <zharkyn@mutinyhq.com>
2023-01-18 19:22:27 -0800

    done

--- src/App.js
+++ src/App.js
@@ -10,22 +10,6 @@ const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
 const LoggedOutComponent = lazy(() => import("./logged_out/components/Main"));
 
 function App() {
-  React.useEffect(() => {
-    const head = document.querySelector("head");
-    const script = document.createElement("script");
-    script.innerHTML = '(function(){var a=window.mutiny=window.mutiny||{};if(!window.mutiny.client){a.client={_queue:{}};var b=["identify","trackConversion"];var c=[].concat(b,["defaultOptOut","optOut","optIn"]);var d=function factory(c){return function(){for(var d=arguments.length,e=new Array(d),f=0;f<d;f++){e[f]=arguments[f]}a.client._queue[c]=a.client._queue[c]||[];if(b.includes(c)){return new Promise(function(b,d){a.client._queue[c].push({args:e,resolve:b,reject:d});setTimeout(d,500)})}else{a.client._queue[c].push({args:e})}}};c.forEach(function(b){a.client[b]=d(b)})}})();'
-    head.appendChild(script);
-
-    const script2 = document.createElement("script");
-    script2.setAttribute("src", "https://client-registry.mutinycdn.com/personalize/client/edc2581ddb9d09bd.js");
-    script2.setAttribute("data-cfasync", "false")
-
-    return () => {
-      head.removeChild(script);
-      head.removeChild(script2);
-    };
-  }, []);
-
   return (
     <HashRouter>
       <MuiThemeProvider theme={theme}>
